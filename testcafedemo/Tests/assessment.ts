b0486b43e4fb4924923c3c5b3fe081156b9f4a53
Pavani Arandara <88416866+IT20174576@users.noreply.github.com>
2023-04-02 13:12:31 +0530

    Update assessment.ts

--- testcafedemo/Tests/assessment.ts
+++ testcafedemo/Tests/assessment.ts
@@ -1,9 +1,6 @@
 import { Selector, t } from 'testcafe';
 import { faker } from '@faker-js/faker';
 
-const PRODUCT_NAME = 'Sauce Labs Fleece Jacket';
-const PRODUCT_PRICE = '$49.99';
-
 class LoginPage {
     public usernameInput: Selector;
     public passwordInput: Selector;
@@ -20,6 +17,27 @@ class LoginPage {
              .click(this.loginButton);
     }
   }
+  
+  class ProductPage {
+    public productPrice: Selector;
+    public addToCartButtons: Selector;
+    //public cartIcon: Selector;
+  
+    constructor() {
+      this.productPrice = Selector('.inventory_item_price').withText('$49.99');
+      this.addToCartButtons = Selector('.btn_primary.btn_inventory');
+      //this.cartIcon = Selector('.shopping_cart_link');
+    }
+  
+    public async addProductsToCart() {
+       await t.click('#add-to-cart-sauce-labs-backpack')
+              .click('#add-to-cart-sauce-labs-bike-light');
+    }
+  
+    public async checkout() {
+      await t.click(Selector('.btn_action.checkout_button'));
+    }
+  }
 
 fixture ('Testing')
     .page ('https://www.saucedemo.com')
@@ -33,63 +51,58 @@ fixture ('Testing')
 //Test case 2 - Check the price of product, Sauce Labs Fleece Jacket is $49.99
 test('Check the price of product, Sauce Labs Fleece Jacket is $49.99', async t =>{
    
-  // check the price of product
-  const product = await Selector('.inventory_item_name').withExactText(PRODUCT_NAME);
-  const price = await product.sibling('.inventory_item_price').withText(PRODUCT_PRICE);
-
+    const productPage = new ProductPage();
+    await t.expect(productPage.productPrice.exists).ok()
+           .expect(productPage.productPrice.innerText).eql('$49.99');
 });
 
 //Test case 3 - Add any two products into the cart
 test('Add any two products into the cart', async t =>{
-      
-    // Add any two products into the cart 
-await t
-    .click('#add-to-cart-sauce-labs-backpack')
-    .click('#add-to-cart-sauce-labs-bike-light');
+
+        // Add any two products into the cart 
+         const productPage = new ProductPage();
+         await productPage.addProductsToCart();
 });
 
 //Test case 4 - Click cart icon in the top 
 test('Click cart icon in the top', async t =>{
-await t
-    .click('#add-to-cart-sauce-labs-backpack')
-    .click('#add-to-cart-sauce-labs-bike-light')
 
-        //Click cart icon in the top 
-        .click('#shopping_cart_container');
-        
+        const productPage = new ProductPage();
+        await productPage.addProductsToCart();
+
+        //Click cart icon in the top */
+       await t.click('#shopping_cart_container');      
     });
  
 //Test case 5 -Verify the items in the cart and the checkout button
 test('Verify the items in the cart and click the ckeckout button', async t =>{
    
-await t
-    .click('#add-to-cart-sauce-labs-backpack')
-    .click('#add-to-cart-sauce-labs-bike-light')
-//verify the item in the cart
-        .click('.shopping_cart_link')
-        .expect(Selector('.cart_item').count).eql(2);
+        const productPage = new ProductPage();
+        await productPage.addProductsToCart();
+        await t.click('#shopping_cart_container');   
 
-//click the checkout button
-await t
-.click('#shopping_cart_container')
-.click('#checkout');
+        //verify the item in the cart
+            await t.click('.shopping_cart_link')
+            .expect(Selector('.cart_item').count).eql(2);
 
+        //click the checkout button
+        await productPage.checkout();
 });
 
 //Test case 6 - Provide a random firstname, lastname and a zip code in the next page 
 test('Provide a random firstname, lastname and a zip code in the next page ', async t =>{
       
-         //add products into the cart
-    await t
-        .click('#add-to-cart-sauce-labs-backpack')
-        .click('#add-to-cart-sauce-labs-bike-light')
-            .click('#shopping_cart_container')
+            //add products into the cart
+            const productPage = new ProductPage();
+            await productPage.addProductsToCart();
+    
+            await t.click('#shopping_cart_container');   
 
-             //Click the checkout button and provide random data using faker
-            .click('#checkout')
-            .typeText('#first-name', faker.name.firstName())
-            .typeText('#last-name', faker.name.lastName())
-            .typeText('#postal-code', faker.address.zipCode())
+            //Click the checkout button and provide random data using faker
+            await productPage.checkout();
+            await t.typeText('#first-name', faker.name.firstName())
+                   .typeText('#last-name', faker.name.lastName())
+                   .typeText('#postal-code', faker.address.zipCode())
 
             // click the continue button
             .click('#continue')
